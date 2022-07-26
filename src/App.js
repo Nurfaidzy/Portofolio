e96b7b57f964f7d5555e0352091cebaf513996eb
nurfaidzy <nurfaidzy@gmail.com>
2022-07-26 11:10:50 +0700

    add Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Perkenalan from "./Components/Perkenalan";
 import Portopolio from "./Components/Portopolio";
@@ -6,13 +7,19 @@ import Project from "./Components/Project";
 
 function App() {
   return (
-    <div className="px-[10%] laptop:py-[5%] bg-slate-200 h-[100%] text-slate-800">
-      <Navbar />
-      <Perkenalan />
-      <Portopolio />
-      <Project />
-      <Contact />
-    </div>
+    <>
+      <div className="px-[10%] laptop:pt-[5%] pb-10 bg-slate-200 h-[100%] text-slate-800">
+        <Navbar />
+        <Perkenalan />
+        <Portopolio />
+        <Project />
+        <Contact />
+      </div>
+
+      <div className="bg-slate-500 ">
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="py-10">
+        <div className="bg-slate-500 font-bold text-center text-white">
+          i do this with love
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
